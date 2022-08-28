f6de6c7191121dbfb98289ed735511a7cacc8e35
Pratik Pimpalkar <pratikpimpalkar21@gmail.com>
2022-08-28 13:32:53 +0530

    Events Javascript added

--- scripts/timeline.js
+++ scripts/timeline.js
@@ -1,5 +1,82 @@
 
 document.getElementById("firCon").onclick = function () {
-    const child = document.getElementById("secCon")
-    child.classList.toggle("toggleDisplay")
+    // const child = document.getElementById("secCon")
+    const child = document.querySelectorAll("#secCon")
+    const child2 = document.querySelectorAll("#secCon2")
+    const child3 = document.querySelectorAll("#secCon3")
+    const child4 = document.querySelectorAll("#secCon4")
+    const child5 = document.querySelectorAll("#secCon5")
+    console.log(child);
+    for (var i = 0; i < child.length; i++) {
+        child[i].classList.toggle("toggleDisplay")
+        child2[i].classList.add("toggleDisplay")
+        child3[i].classList.add("toggleDisplay")
+        child4[i].classList.add("toggleDisplay")
+        child5[i].classList.add("toggleDisplay")
+    }
+}
+document.getElementById("firCon2").onclick = function () {
+    // const child = document.getElementById("secCon")
+    const child = document.querySelectorAll("#secCon")
+    const child2 = document.querySelectorAll("#secCon2")
+    const child3 = document.querySelectorAll("#secCon3")
+    const child4 = document.querySelectorAll("#secCon4")
+    const child5 = document.querySelectorAll("#secCon5")
+    console.log(child);
+    for (var i = 0; i < child.length; i++) {
+        child[i].classList.add("toggleDisplay")
+        child2[i].classList.toggle("toggleDisplay")
+        child3[i].classList.add("toggleDisplay")
+        child4[i].classList.add("toggleDisplay")
+        child5[i].classList.add("toggleDisplay")
+    }
+}
+
+document.getElementById("firCon3").onclick = function () {
+    // const child = document.getElementById("secCon")
+    const child = document.querySelectorAll("#secCon")
+    const child2 = document.querySelectorAll("#secCon2")
+    const child3 = document.querySelectorAll("#secCon3")
+    const child4 = document.querySelectorAll("#secCon4")
+    const child5 = document.querySelectorAll("#secCon5")
+    console.log(child);
+    for (var i = 0; i < child.length; i++) {
+        child[i].classList.add("toggleDisplay")
+        child2[i].classList.add("toggleDisplay")
+        child3[i].classList.toggle("toggleDisplay")
+        child4[i].classList.add("toggleDisplay")
+        child5[i].classList.add("toggleDisplay")
+    }
+}
+document.getElementById("firCon4").onclick = function () {
+    // const child = document.getElementById("secCon")
+    const child = document.querySelectorAll("#secCon")
+    const child2 = document.querySelectorAll("#secCon2")
+    const child3 = document.querySelectorAll("#secCon3")
+    const child4 = document.querySelectorAll("#secCon4")
+    const child5 = document.querySelectorAll("#secCon5")
+    console.log(child);
+    for (var i = 0; i < child.length; i++) {
+        child[i].classList.add("toggleDisplay")
+        child2[i].classList.add("toggleDisplay")
+        child3[i].classList.add("toggleDisplay")
+        child4[i].classList.toggle("toggleDisplay")
+        child5[i].classList.add("toggleDisplay")
+    }
+}
+document.getElementById("firCon5").onclick = function () {
+    // const child = document.getElementById("secCon")
+    const child = document.querySelectorAll("#secCon")
+    const child2 = document.querySelectorAll("#secCon2")
+    const child3 = document.querySelectorAll("#secCon3")
+    const child4 = document.querySelectorAll("#secCon4")
+    const child5 = document.querySelectorAll("#secCon5")
+    console.log(child);
+    for (var i = 0; i < child.length; i++) {
+        child[i].classList.add("toggleDisplay")
+        child2[i].classList.add("toggleDisplay")
+        child3[i].classList.add("toggleDisplay")
+        child4[i].classList.add("toggleDisplay")
+        child5[i].classList.toggle("toggleDisplay")
+    }
 }
